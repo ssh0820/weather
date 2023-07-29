69c0f57a376a6db81abb500bad1855855a72cc60
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2023-07-29 22:16:28 +0900

    diary 초기세팅

---
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -0,0 +1,26 @@
+package zerobase.weather.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String weather;
+    private String icon;
+    private String text;
+    private double temperature;
+    private LocalDate date;
+
+}
